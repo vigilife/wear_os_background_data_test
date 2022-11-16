ab13fa624b7665c45e46f1f24c3a4791e30c1ad4
Rexios <rexios@rexios.dev>
2022-11-16 11:33:00 -0500

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:noise_meter/noise_meter.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:wear/wear.dart';
 import 'package:workout/workout.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final workout = Workout();
 
-  await FlutterLocalNotificationsPlugin()
+  final flnp = FlutterLocalNotificationsPlugin();
+
+  await flnp.initialize(
+    const InitializationSettings(
+      android: AndroidInitializationSettings('ic_notification_icon'),
+    ),
+  );
+
+  await flnp
       .resolvePlatformSpecificImplementation<
           AndroidFlutterLocalNotificationsPlugin>()!
-      .startForegroundService(123, 'test', 'test');
+      .startForegroundService(
+    123,
+    'test',
+    'test',
+    notificationDetails: const AndroidNotificationDetails('test', 'test'),
+    foregroundServiceTypes: {
+      AndroidServiceForegroundType.foregroundServiceTypeMicrophone
+    },
+  );
 
   await workout.start(
     exerciseType: ExerciseType.walking,
     features: WorkoutFeature.values,
   );
 
-  NoiseMeter().noiseStream.listen((noise) {
-    debugPrint(noise.toString());
-  });
+  workout.stream.listen((e) => debugPrint('${e.feature}: ${e.value}'));
+
+  accelerometerEvents
+      .throttle((_) => TimerStream(true, const Duration(seconds: 1)))
+      .listen(((event) => debugPrint(event.toString())));
 
   runApp(const MyApp());
 }
@@ -28,8 +50,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(child: Text('Hello, world!')),
+    return MaterialApp(
+      home: AmbientMode(
+        builder: (context, mode, child) => child!,
+        child: const Scaffold(body: Center(child: Text('Hello, world!'))),
+      ),
     );
   }
 }
