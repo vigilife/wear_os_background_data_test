5aaf899bd7f3f4ce44e3179b2532977c96cb3b94
Rexios <rexios@rexios.dev>
2023-03-15 16:00:12 -0400

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -5,33 +5,53 @@ import 'package:wear/wear.dart';
 import 'package:workout/workout.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final workout = Workout();
-  await workout.start(
-    exerciseType: ExerciseType.walking,
-    features: WorkoutFeature.values,
-  );
-
-  workout.stream.listen((e) => debugPrint('${e.feature}: ${e.value}'));
-
-  // Comment this out to see Health Services behave normally
-  accelerometerEvents
-      .throttle((_) => TimerStream(true, const Duration(seconds: 1)))
-      .listen(((event) => debugPrint(event.toString())));
-
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var heartRate = 0.0;
+
+  @override
+  void initState() async {
+    super.initState();
+
+    final workout = Workout();
+    await workout.start(
+      exerciseType: ExerciseType.walking,
+      features: WorkoutFeature.values,
+    );
+
+    workout.stream.listen((e) {
+      debugPrint('${e.feature}: ${e.value}');
+
+      if (e.feature == WorkoutFeature.heartRate) {
+        setState(() => heartRate = e.value);
+      }
+    });
+
+    // Comment this out to see Health Services behave normally
+    accelerometerEvents
+        .throttle((_) => TimerStream(true, const Duration(seconds: 1)))
+        .listen((event) => debugPrint(event.toString()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: AmbientMode(
         builder: (context, mode, child) => child!,
-        child: const Scaffold(body: Center(child: Text('Hello, world!'))),
+        child: Scaffold(
+          body: Center(
+            child: Text('Heart rate: $heartRate'),
+          ),
+        ),
       ),
     );
   }
